5175c5602a3a0b0d78f47dce9b14d1ac4b20ae2b
Boryana Dinkova <boryana.dinkova@gmail.com>
2020-07-27 16:10:33 +0300

    Message alert bugfix 3

--- public/js/index.js
+++ public/js/index.js
@@ -109,4 +109,4 @@ if (reviewForm)
 
 const alertMessage = document.querySelector('body').dataset.alert;
 
-if (alert) showAlert('success', alertMessage);
+if (alertMessage) showAlert('success', alertMessage);
