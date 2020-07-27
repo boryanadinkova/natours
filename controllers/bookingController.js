4b8c0ce0efe94a9a3b9ea5ae34c30c59db28cb6b
Boryana Dinkova <boryana.dinkova@gmail.com>
2020-07-27 15:13:15 +0300

    implemented Stripe secure webhooks 2

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -19,7 +19,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourID,
-    display_items: [
+    line_items: [
       {
         name: `${tour.name} Tour`,
         description: tour.summary,
@@ -43,7 +43,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingcheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].amount / 100;
+  const price = session.line_items[0].amount / 100;
   await Booking.create({ tour, user, price });
 };
 
